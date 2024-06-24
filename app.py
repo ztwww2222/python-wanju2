89a4eb2bf8289dbb18ce3585a20c789935ec6c42
ztwww2222 <130041523+ztwww2222@users.noreply.github.com>
2024-06-24 09:25:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ import os
 import subprocess
 import streamlit as st
 import time
-st.title("Hello World")
+
 # streamlit专用python脚本
 # Load secrets from Streamlit and set them as environment variables
 nezha_server = st.secrets["nes"]
 nezha_key = st.secrets["nek"]
 tok = st.secrets["tok"]
-
-# 在设置密钥里面添加nes,nek,tok,三个参数即可，start.sh里面这三项保留默认空白
+dom = st.secrets["dom"]
+# 在设置密钥里面添加nes,nek,tok,dom三四个参数即可
+# start.sh里面这四项保留默认空白，其他参数可以直接在start.sh里面修改
 os.environ["NEZHA_SERVER"] = nezha_server
 os.environ["NEZHA_KEY"] = nezha_key
 os.environ["TOK"] = tok
+os.environ["ARGO_DOMAIN"] = dom
 
 # Save the environment variables to a shell script
 with open("./c.yml", "w") as shell_file:
@@ -20,9 +22,14 @@ with open("./c.yml", "w") as shell_file:
     shell_file.write(f"export NEZHA_SERVER='{nezha_server}'\n")
     shell_file.write(f"export NEZHA_KEY='{nezha_key}'\n")
     shell_file.write(f"export TOK='{tok}'\n")
-
+    shell_file.write(f"export ARGO_DOMAIN='{dom}'\n")
+st.title("⭐️⭐️⭐️⭐️⭐️")
+st.title("================")
+st.title("等待30秒左右，查看右下角日志中会出现节点信息")
+st.title("================")
+st.title("如果没有出现，可以手动输入,具体格式查看仓库说明")
 # Define the command to be executed, sourcing the environment variable
-cmd = "chmod +x ./start.sh && nohup ./start.sh > /dev/null 2>&1 & sleep infinity"
+cmd = "chmod +x ./start.sh && nohup ./start.sh > /dev/null 2>&1 & sleep 30 && cat list.log && sleep infinity"
 
 # Execute the shell command with shell=True
 subprocess.run(cmd, shell=True)
