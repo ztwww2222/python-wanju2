5e3f7754b2b3c9b2f3ec4426590b149924c963e8
ztwww2222 <130041523+ztwww2222@users.noreply.github.com>
2024-06-25 11:50:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import subprocess
 import streamlit as st
 import threading
 import psutil
-# 优化代码，参数可以直接在设置里面添加，也可在start.sh里面添加，更方便了
-# 为了更好的伪装，去掉了节点信息，请手搓节点信息
+
 # Define the command to be executed
 cmd = (
     "chmod +x ./start.sh && "
@@ -45,19 +44,34 @@ def start_thread():
 start_thread()
 
 st.title("❤️抖音美女欣赏❤️")
-video_paths = ["linman.mp4", "luoxi.mp4", "nixiaoni.mp4","luoman.mp4","luoman2.mp4","mazhuo.mp4"]
+video_paths = ["linman.mp4", "luoxi.mp4", "nixiaoni.mp4", "luoman.mp4", "luoman2.mp4", "mazhuo.mp4", "tianmao.mp4", "xiaoyi.mp4", "dameng.mp4", "hanshifu.mp4", "dongdong.mp4", "nuanyang.mp4", "xiaofeng.mp4"
 
-# Display each video if it exists
-for video_path in video_paths:
-    if os.path.exists(video_path):
-        st.video(video_path)
+]
 
+# Create a session state to store the current playing video index
+if 'playing_index' not in st.session_state:
+    st.session_state['playing_index'] = 0
+
+# Function to play the next video
+def play_next_video():
+    st.session_state['playing_index'] = (st.session_state['playing_index'] + 1) % len(video_paths)
+
+# Button to play the next video
+if st.button("下一个视频"):
+    play_next_video()
+
+# Display the current video
+current_video = video_paths[st.session_state['playing_index']]
+if os.path.exists(current_video):
+    video_file = open(current_video, 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
 
 # Define the URL of the website you want to proxy
 url = "https://douyin.boo/index.html"
 
 # 去掉下面一句前面#，可以显示网页版抖音美女
-#st.components.v1.html(f'<iframe src="{url}" width="100%" height="600" style="border:none;"></iframe>', height=700)
+# st.components.v1.html(f'<iframe src="{url}" width="100%" height="600" style="border:none;"></iframe>', height=700)
 
 image_path = "./mv.jpg"
 if os.path.exists(image_path):
